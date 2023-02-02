221fb214c3c6310b0faf256d1d1fb192f5baf819
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-02-02 23:50:13 +0000

    This first example is swift 101 code before he starts explaining that although its very common to see this type of thing it's not ideal.

--- PerformanceSandBox/ContentView.swift
+++ PerformanceSandBox/ContentView.swift
@@ -7,50 +7,84 @@
 import Combine
 import SwiftUI
 
-class Debouncer<T>: ObservableObject {
-    @Published var input: T
-    @Published var output: T
-    
-    private var debounce: AnyCancellable?
-    
-    init(initialValue: T, delay: Double = 1) {
-        self.input = initialValue
-        self.output = initialValue
-        
-        debounce = $input
-            .debounce(for: .seconds(delay), scheduler: DispatchQueue.main)
-            .sink { [weak self] in
-                self?.output = $0
-            }
-    }
-}
-
+//class Debouncer<T>: ObservableObject {
+//    @Published var input: T
+//    @Published var output: T
+//
+//    private var debounce: AnyCancellable?
+//
+//    init(initialValue: T, delay: Double = 1) {
+//        self.input = initialValue
+//        self.output = initialValue
+//
+//        debounce = $input
+//            .debounce(for: .seconds(delay), scheduler: DispatchQueue.main)
+//            .sink { [weak self] in
+//                self?.output = $0
+//            }
+//    }
+//}
+//
 class SaveData: ObservableObject {
     @Published var highScore = 0
 }
+
+//struct ContentView: View {
+//    @StateObject @DebouncedObservedObject var saveData = SaveData()
+//
+//    var body: some View {
+//        VStack {
+//            Button("High Score: \(saveData.highScore )") {
+//                saveData.highScore += 1
+//            }
+//            OtherView(saveData: saveData)
+//        }
+//    }
+//}
+//
+//struct OtherView: View {
+//    @ObservedObject var saveData: SaveData
+//
+//    var body: some View {
+//        Text("Your score is: \(saveData.highScore)")
+//    }
+//}
+
+struct DisplayingView: View {
+    @EnvironmentObject var saveData: SaveData
+    
+    var body: some View {
+        Text("Your high score is \(saveData.highScore)")
+    }
+}
+
+struct UpdatingView: View {
+    @EnvironmentObject var saveData: SaveData
+    
+    var body: some View {
+        Button("Add to high score") {
+            saveData.highScore += 1
+        }
+        
+    }
+}
+
 struct ContentView: View {
-    @StateObject @DebouncedObservedObject var saveData = SaveData()
+    @StateObject var saveData = SaveData()
     
     var body: some View {
         VStack {
-            Button("High Score: \(saveData.highScore )") {
-                saveData.highScore += 1
-            }
-            OtherView(saveData: saveData)
+            DisplayingView()
+            UpdatingView()
         }
+        .environmentObject(saveData)
     }
 }
 
-struct OtherView: View {
-    @ObservedObject var saveData: SaveData
-    
-    var body: some View {
-        Text("Your score is: \(saveData.highScore)")
-    }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
