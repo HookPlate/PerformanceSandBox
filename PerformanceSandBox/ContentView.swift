3ec87c65c31602f22d55167693fc1e94b9cf4ab5
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-02-02 12:49:58 +0000

    Gets rid of the notes from last commit.

--- PerformanceSandBox/ContentView.swift
+++ PerformanceSandBox/ContentView.swift
@@ -29,12 +29,9 @@ class SaveData: ObservableObject {
     @Published var highScore = 0
 }
 struct ContentView: View {
-    //here is were we make the whole object debounced
     @StateObject var saveData = DebouncedObservedObject(wrappedValue: SaveData())
     
     var body: some View {
-        //then we refer to the wrapped value, not just high score. The thing inside the DOO.
-        //To see this in action run the app and click fast on the button, the change only happens after one second of inactivity.
         Button("High Score: \(saveData.wrappedValue.highScore )") {
             saveData.wrappedValue.highScore += 1
         }
