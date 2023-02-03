3f5f5e092066be9068e0f089360e53779bb9be78
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-02-03 11:04:20 +0000

    This approach creates a custom EnvironmentKey allows us to place a reference type (object/class) into the environment as a struct value type, then extending the getters and setters inside EnvironmentValues so we can us @Environment(/ourCustomKeyName) for a view that only wants to post/write data and doesn't care about any updates. Apparently this is in ProSwiftUI too.

--- PerformanceSandBox/ContentView.swift
+++ PerformanceSandBox/ContentView.swift
@@ -7,50 +7,13 @@
 import Combine
 import SwiftUI
 
-//class Debouncer<T>: ObservableObject {
-//    @Published var input: T
-//    @Published var output: T
-//
-//    private var debounce: AnyCancellable?
-//
-//    init(initialValue: T, delay: Double = 1) {
-//        self.input = initialValue
-//        self.output = initialValue
-//
-//        debounce = $input
-//            .debounce(for: .seconds(delay), scheduler: DispatchQueue.main)
-//            .sink { [weak self] in
-//                self?.output = $0
-//            }
-//    }
-//}
-//
 class SaveData: ObservableObject {
     @Published var highScore = 0
 }
 
-//struct ContentView: View {
-//    @StateObject @DebouncedObservedObject var saveData = SaveData()
-//
-//    var body: some View {
-//        VStack {
-//            Button("High Score: \(saveData.highScore )") {
-//                saveData.highScore += 1
-//            }
-//            OtherView(saveData: saveData)
-//        }
-//    }
-//}
-//
-//struct OtherView: View {
-//    @ObservedObject var saveData: SaveData
-//
-//    var body: some View {
-//        Text("Your score is: \(saveData.highScore)")
-//    }
-//}
 
 struct DisplayingView: View {
+    //so with this new approach this view wants to read and be notified of changes so it stays as is
     @EnvironmentObject var saveData: SaveData
     
     var body: some View {
@@ -62,10 +25,12 @@ struct DisplayingView: View {
         print("In DisplayingView.init")
     }
 }
- 
+// this view needs to write the data but never needs to read the data. Reading is handled in the other view. We changed some value somewhere and that's reflected somewhere else in our UI but this doesn't care if it changes and that's our mistake. We've told SwiftUI to watch it for changes and reinvoke when it does.
 struct UpdatingView: View {
-    @EnvironmentObject var saveData: SaveData
-    //her needs the return to return only one view I think
+    //since here we want 'write only' data, without watching for changes, so here we want @Environment with the key we made, there's no type needed here, it comes from the environment automatically. This now means we need to inject this into the environment but this time as an environment value
+//    @EnvironmentObject var saveData: SaveData
+    @Environment(\.saveData) var saveData
+
     var body: some View {
         print("In UpdatingView.body")
         return Button("Add to high score") {
@@ -79,8 +44,8 @@ struct UpdatingView: View {
 }
 
 struct ContentView: View {
-    //the below was very performance heavy, the issue was we were telling SwiftUI to observe an object that we didn’t actually use inside ContentView.
-   // @StateObject var saveData = SaveData()
+    //Yes, that’s designed to hold value types rather than reference types, but in this instance it does exactly the same as using environment values rather than environment objects: it holds a reference to the object (keeps it alive), but doesn’t watch it for changes. If you changed teh whole object for a new SaveData() sure it would see that but changing values inside the class won't be seen so it won't reinvoke the body. Just know that we need to use @State now.
+    @State var saveData = SaveData()
     
     var body: some View {
         print("In ContentView.body")
@@ -88,13 +53,34 @@ struct ContentView: View {
             DisplayingView()
             UpdatingView()
         }
-        //We can get rid of @StateObject because this will keep it alive for as long as this view is alive. We post it into the Environment without observing it, we don't want to know if it's changing or not. We can inject it for our child views to use, without making our own view get reloaded when it posts a change notification
-        .environmentObject(SaveData())
+        //so this kind of code wont work anymore, we need to store a reference to it somewhere and inject it from there. See the new @State in ContView above
+        //.environmentObject(SaveData())
+        //now we send that as both an object and a key into the environment - crucial this bit.
+        .environmentObject(saveData)
+        .environment(\.saveData, saveData)
+        
     }
     init() {
         print("In ContentView.init")
     }
 }
+
+//allows us to place a reference type (object/class) into the environment as a struct/value type.
+struct SaveDataKey: EnvironmentKey {
+    //with EnvironmentKeys you must provide a default value, so there's always a defualt value in there for us. So if there isn't one in the environment use this one.
+    static var defaultValue = SaveData()
+}
+
+//now we can make an extension to handle reading and writing these things.
+extension EnvironmentValues {
+    var saveData: SaveData {
+        //to read it we'll read the environment values for our SaveDataKey
+        get { self[SaveDataKey.self] }
+        //to write it we'll change our environment values (at SaveDataKey) to be our new value.
+        set { self[SaveDataKey.self] = newValue }
+    }
+}
+
 
 
 //
